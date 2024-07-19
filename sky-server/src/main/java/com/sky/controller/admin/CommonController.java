27c72858eb78d5fa5f35cc93bc8dff48cd6e88c1
Shen Jia <1984980516@qq.com>
2024-07-19 15:33:18 +0800

    新增菜品业务代码

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 通用接口
@@ -36,14 +38,18 @@ public class CommonController {
             //原始文件名
             String originalFilename = file.getOriginalFilename();
             //截取原始文件名的后缀 dfdfdf.png
-            originalFilename.substring(originalFilename.lastIndexOf("."));
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //构建新文件的名称
+            String objectName = UUID.randomUUID().toString() + extension;
 
-            aliOssUtil.upload(file.getBytes(), null);
+            //文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return  Result.success(filePath);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("文件上传失败；{}",e);
         }
 
 
-        return null;
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
