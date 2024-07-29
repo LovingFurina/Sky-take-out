dfcbd43108437812f97f0cac2b74c2a40a75ccf9
Shen Jia <1984980516@qq.com>
2024-07-29 23:30:42 +0800

    用户数量统计功能开发

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 统计指定时间区间内的营业额数据
@@ -68,8 +73,54 @@ public class ReportServiceImpl implements ReportService {
 
     }//构建VO对象中的dateList
 
+    /**
+     * 统计指定时间区间内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //存放从begin到end之间每天对应的日期
+        List<LocalDate> dateList = new ArrayList<>();
 
+        dateList.add(begin);
+        while(!begin.equals(end)){
+            //日期计算，计算指定日期的后一天对应的日期
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }//通过循环从开始到结束的时候
 
+        //存放每天的新增用户数量 select count(id) from user where create_time < ? and create_time > ?
+        List<Integer> newUserList = new ArrayList<>();
+        //存放每天的总用户数量 select count(id) from user where create_time < ?
+        List<Integer> totalUserList = new ArrayList<>();
 
+        for(LocalDate date : dateList){
+            //查询date日期对应的营业额数据，营业额是指：状态为“已完成”的订单金额合计
+            LocalDateTime beginTime = LocalDateTime.of(date,LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date,LocalTime.MAX);
 
+            //select sum(amount) from orders where order_time > beginTime and order_time < endTime and status =5
+            Map map = new HashMap<>();
+            //先查总用户数量，因为限制条件少
+            map.put("end",endTime);//这里统计总用户数量
+
+            Integer totalUser = userMapper.countByMap(map);
+
+            map.put("begin",beginTime);
+            //新增用户数量
+            Integer newUser =  userMapper.countByMap(map);
+
+            totalUserList.add(totalUser);
+            newUserList.add(newUser);
+        }
+
+        //封装结果数据
+        return UserReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList,","))
+                .totalUserList(StringUtils.join(totalUserList,","))
+                .newUserList(StringUtils.join(newUserList,","))
+                .build();
+    }
 }
